01dd21a44f73a1e2092a31ed90b799c285a69fea
Saif Ahmed <saif.aahmed903@gmail.com>
2022-12-23 10:43:53 +0000

    registerbutton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,26 +3,35 @@ import SearchForm from './components/searchForm';
 import SearchResults from './components/searchResults';
 import LoginForm from './components/login';
 import RegisterForm from './components/register';
-import Navbar from './components/navbar';
-import './App.css';
 
 function App() {
   // State for the search results
   const [results, setResults] = useState([]);
+  
+  // State to toggle the register form button
+
+  state = {
+    showRegisterForm: false,
+  };
+
+  toggleRegisterForm = () => {
+    this.setState((prevState) => ({
+      showRegisterForm: !prevState.showRegisterForm,
+    }));
+  };
 
   return (
-  <div>
-    <header>
-    </header>
-    <main>
+    <div>
       <SearchForm onSearch={setResults} />
       <SearchResults results={results} />
       <LoginForm/>
-      <RegisterForm/>
-      </main>
-    <footer>
-    </footer>
-  </div>
+      
+
+      <button onClick={this.toggleRegisterForm}>
+          {this.state.showRegisterForm ? 'Hide' : 'Show'} Register Form
+        </button>
+        {this.state.showRegisterForm && <RegisterForm />}
+    </div>
   );
 }
 
